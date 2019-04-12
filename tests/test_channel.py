5ad1033c8ab7c7a36cd1cb5ba96815abcdde5779
Jonne Kaunisto <jonneka@gmail.com>
2019-04-11 21:51:27 -0700

    changed test for uploading

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,21 +1,25 @@
 from simple_youtube_api.Channel import Channel
-from simple_youtube_api.Video import Video
+from simple_youtube_api.LocalVideo import LocalVideo
 
-import urllib.request
-
+import urllib
 import pytest
 import os
+import datetime
 
 VIDEO_RESOURCE_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+VIDEO_NAME = "test_video.mp4"
 
 def download_video():
-    urllib.request.urlretrieve(url_link, VIDEO_RESOURCE_URL) 
+    urllib.request.urlretrieve(VIDEO_RESOURCE_URL, VIDEO_NAME) 
 
 def test_channel_regular_function():
+    title = "Time: " + datetime.datetime.now()
+    download_video()
+
     channel = Channel()
     channel.login("credentials/client_secret.json", "credentials/credentials.storage")
-    video = Video(file_path="ForBiggerBlazes.mp4")
-    video.set_title("This is a title")
+    video = LocalVideo(file_path=VIDEO_NAME)
+    video.set_title(title)
     video.set_description("This is a description")
     video.set_tags(["this", "tag"])
     video.set_category("film")
