01148e386e178debbf0d72ecfb06a51ae59b6a76
Jonne Kaunisto <jonneka@gmail.com>
2019-04-11 22:08:58 -0700

    added asserts

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -8,16 +8,21 @@ import datetime
 
 VIDEO_RESOURCE_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
 VIDEO_NAME = "test_video.mp4"
+CLIENT_SECRET_NAME = "credentials/client_secret.json"
+CREDENTIALS = "credentials/credentials.storage"
 
 def download_video():
     urllib.request.urlretrieve(VIDEO_RESOURCE_URL, VIDEO_NAME) 
 
 def test_channel_regular_function():
+    assert os.path.isfile(CLIENT_SECRET_NAME), "CLIENT SECRET_NAME is not valid"
+    assert os.path.isfile(CREDENTIALS), "CREDENTIALS is not valid"
+
     title = "Time: " + str(datetime.datetime.now())
     download_video()
 
     channel = Channel()
-    channel.login("credentials/client_secret.json", "credentials/credentials.storage")
+    channel.login(CLIENT_SECRET_NAME, CREDENTIALS)
     video = LocalVideo(file_path=VIDEO_NAME)
     video.set_title(title)
     video.set_description("This is a description")
