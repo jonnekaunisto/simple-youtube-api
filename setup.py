343f8824e8b5706101503a9e4ae815dfd27037f6
Jonne Kaunisto <jonneka@gmail.com>
2019-06-28 22:26:24 -0700

    updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.test import test as TestCommand
 
 MAJOR               = 0
 MINOR               = 2
-MICRO               = 0
+MICRO               = 1
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
