698c1e617b5fbbbe249f5098c51a7cc4a7d96b2a
Jonne Kaunisto <jonneka@gmail.com>
2019-07-05 20:36:39 -0700

    changed docs version and removed itself from requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Jonne Kaunisto'
 author = 'Jonne Kaunisto'
 
 # The short X.Y version
-version = ''
+version = '0.2.2'
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
